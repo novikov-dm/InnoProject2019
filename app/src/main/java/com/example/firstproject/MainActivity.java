e08e671e2fa3caf83395549d022ac70d8f0b7ac0
Daaaaaxa <dashaaarhipova5@gmail.com>
2019-08-15 13:57:05 +0300

    Изменения

--- app/src/main/java/com/example/firstproject/MainActivity.java
+++ app/src/main/java/com/example/firstproject/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.firstproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -38,6 +39,12 @@ public class MainActivity extends AppCompatActivity {
 
     String QUESTION = "-%d * (%d - %d)";
 
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
