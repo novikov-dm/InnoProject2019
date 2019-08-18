838859f854888fae233b4abaa8578893a8a54902
Daaaaaxa <dashaaarhipova5@gmail.com>
2019-08-18 12:12:37 +0300

    добавили задачи

--- app/src/main/java/com/example/firstproject/InfoActivity.java
+++ app/src/main/java/com/example/firstproject/InfoActivity.java
@@ -1,7 +1,11 @@
 package com.example.firstproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class InfoActivity extends AppCompatActivity {
@@ -9,5 +13,23 @@ public class InfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.tasks:
+                Intent intent = new Intent(InfoActivity.this, MainActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
