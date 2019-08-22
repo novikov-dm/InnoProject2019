d2ece99ce463f91739d7b1b6572efc2f866ad25a
Dmitri M. Novikov <e-main@novikov-dm.ru>
2019-08-22 12:17:57 +0300

    Добавлены задачи

--- app/src/main/java/ru/novikov_dm/studystraus/MainActivity.java
+++ app/src/main/java/ru/novikov_dm/studystraus/MainActivity.java
@@ -13,9 +13,6 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-    //private int nProblems = 7;
-    //private Problem[] problems = new Problem[nProblems];
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,30 +24,9 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
-
-
-    public void toProblems1(View view) {
-
-
-        //problems[0] = new Problem1();
-        //problems[1] = new Problem2();
-        //problems[2] = new Problem3();
-        //problems[3] = new Problem4();
-        //problems[4] = new Problem5();
-        //problems[5] = new Problem7();
-        //problems[6] = new Problem8();
-
-        Problems problems = new Problems(new Problem1(), new Problem2(), new Problem3(), new Problem4(), new Problem5());
-
-        Intent intent = new Intent(MainActivity.this, Problems1Activity.class);
-        intent.putExtra(Problems.class.getSimpleName(), problems);
-        startActivity(intent);
-    }
-
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
-
         switch (id) {
             case R.id.about:
                 Intent intent = new Intent(MainActivity.this, InfoActivity.class);
@@ -59,6 +35,12 @@ public class MainActivity extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+
+    public void toProblems1(View view) {
+        Intent intent = new Intent(MainActivity.this, Problems1Activity.class);
+        startActivity(intent);
     }
 
     public void toProblems2(View view) {
